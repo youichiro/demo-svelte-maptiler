6ce93b9b1fc804ffb63942ae07c8c0e3ff900638
youichiro <cinnamon416@gmail.com>
2023-09-14 09:29:51 +0900

    display map

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,18 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  import * as maptilersdk from '@maptiler/sdk';
+
+  maptilersdk.config.apiKey = 'aQhm9yzeHN2EnPoVqMPB';
+  maptilersdk.config.primaryLanguage = maptilersdk.Language.JAPANESE;
+
+  const initMap = (container: HTMLElement) => {
+    const map = new maptilersdk.Map({
+      container: container,
+      style: maptilersdk.MapStyle.STREETS,
+      center: [139.7017, 35.6581], // 渋谷
+      zoom: 12,
+      fullscreenControl: true,
+    });
+  };
+</script>
+
+<div use:initMap style="height: 100vh;" />
