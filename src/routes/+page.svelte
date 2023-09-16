9556ba2db1e6c69d88a1a9868fcaeaf108820b35
youichiro <cinnamon416@gmail.com>
2023-09-16 17:20:30 +0900

    add home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,14 @@
-aaa
+<script lang="ts">
+  const liClass = "text-blue-500 underline"
+</script>
+
+
+<div class="w-[400px] mx-auto mt-16">
+  <h2 class="text-lg mb-4">Maps</h2>
+
+  <ul class="list-disc">
+    <li>
+      <a href="/default" class={liClass}>default map</a>
+    </li>
+  </ul>
+</div>
