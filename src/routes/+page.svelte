67fdf241bb7fb51709b20cf39aa3d9a9056451f0
youichiro <cinnamon416@gmail.com>
2023-09-16 17:11:15 +0900

    add MapDefault.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,5 @@
 <script lang="ts">
-  import * as maptilersdk from '@maptiler/sdk';
-
-  maptilersdk.config.apiKey = 'aQhm9yzeHN2EnPoVqMPB';
-  maptilersdk.config.primaryLanguage = maptilersdk.Language.JAPANESE;
-
-  const initMap = (container: HTMLElement) => {
-    const map = new maptilersdk.Map({
-      container: container,
-      style: maptilersdk.MapStyle.STREETS,
-      center: [139.7017, 35.6581], // 渋谷
-      zoom: 12,
-      fullscreenControl: true,
-    });
-  };
+  import MapDefault from "$lib/MapDefault.svelte";
 </script>
 
-<div use:initMap style="height: 100vh;" />
+<MapDefault />
