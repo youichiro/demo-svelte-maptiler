6104893af89ba2535834e49d1587e0d7b5b20b88
youichiro <cinnamon416@gmail.com>
2023-09-16 18:21:53 +0900

    設定ボタンをクリックしたら通知する

--- src/lib/MapSelectable.svelte
+++ src/lib/MapSelectable.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
   import * as maptilersdk from '@maptiler/sdk';
-  import { Button } from 'flowbite-svelte';
+  import { Alert, Button } from 'flowbite-svelte';
 
   maptilersdk.config.apiKey = 'aQhm9yzeHN2EnPoVqMPB';
   maptilersdk.config.primaryLanguage = maptilersdk.Language.JAPANESE;
 
   let currentMaker: maptilersdk.Marker | null = null;
+  let clicked = false;
 
   const initMap = (container: HTMLElement) => {
     const map = new maptilersdk.Map({
@@ -32,10 +33,23 @@
     });
   };
 
+  const clickedOn = () => {
+    clicked = true
+  }
+
+  const clickedOff = () => {
+    clicked = false
+  }
 </script>
 
 <div use:initMap style="height: 100vh;" />
 
 <div class="fixed bottom-16 left-1/2 transform -translate-x-1/2">
-  <Button disabled={currentMaker === null} color="blue" class="py-2 px-6 rounded-full shadow-lg focus:outline-none">この場所に設定する</Button>
+  <Button disabled={currentMaker === null} on:click={clickedOn} color="blue" class="py-2 px-6 rounded-full shadow-lg focus:outline-none">この場所に設定する</Button>
 </div>
+
+{#if clicked}
+  <div class="fixed top-4 left-1/2 transform -translate-x-1/2">
+    <Alert border dismissable on:close={clickedOff} color="green" class="w-[260px]">👍 設定しました</Alert>
+  </div>
+{/if}
